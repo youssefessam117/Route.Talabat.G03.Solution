92938e0afcfd959e731b7725a964f687df9927f2
youssefessam117 <johntiastoo@gmail.com>
2024-04-11 19:02:55 +0200

    Update Database from program class

--- Route.Talabat.APIs/Program.cs
+++ Route.Talabat.APIs/Program.cs
@@ -6,9 +6,11 @@ namespace Route.Talabat.APIs
 {
 	public class Program
 	{
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			/// bilder is the web application builder that will create the app and the privios name
+			
+
+			/// builder is the web application builder that will create the app and the privios name
 			/// was builder and the new name will be webApplicationBuilder
 			/// and here we are creating the app by call  WebApplication.CreateBuilder(args);
 			var webApplicationBuilder = WebApplication.CreateBuilder(args);
@@ -29,6 +31,26 @@ namespace Route.Talabat.APIs
 
 			var app = webApplicationBuilder.Build();
 
+			using var scop = app.Services.CreateScope();
+
+			var services = scop.ServiceProvider;
+
+			var _dbContext = services.GetRequiredService<StoreContext>();
+			// ask clr for creating object from dbcontext explicitly 
+
+			var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+			try
+			{
+				await _dbContext.Database.MigrateAsync();
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+
+				var logger = loggerFactory.CreateLogger<Program>();
+				logger.LogError(ex, "an error has been occured during apply migration");
+			}
+
 			#region configure() method to configure kestrel middlewares like dot net 5 
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
@@ -40,8 +62,10 @@ namespace Route.Talabat.APIs
 			app.UseHttpsRedirection();
 
 
-			app.MapControllers(); 
+			app.MapControllers();
 			#endregion
+
+		
 
 			app.Run();
 		}
