e4cd91d6c67c99ccd76b80cdf506bfb5eff42ac6
youssefessam117 <johntiastoo@gmail.com>
2024-04-11 17:37:50 +0200

    DbContext, ProductConfigurations,  add Migrations

--- Route.Talabat.APIs/Program.cs
+++ Route.Talabat.APIs/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Talabat.Infrastructure.Data;
+
 namespace Route.Talabat.APIs
 {
 	public class Program
@@ -17,6 +20,11 @@ namespace Route.Talabat.APIs
 			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 			webApplicationBuilder.Services.AddEndpointsApiExplorer();
 			webApplicationBuilder.Services.AddSwaggerGen(); 
+
+			webApplicationBuilder.Services.AddDbContext<StoreContext>(option =>
+			{
+				option.UseSqlServer(webApplicationBuilder.Configuration.GetConnectionString("DefaultConnection"));
+			});
 			#endregion
 
 			var app = webApplicationBuilder.Build();
---
+++ Talabat.Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entites;
+using Talabat.Infrastructure.Data.Config;
+
+namespace Talabat.Infrastructure.Data
+{
+	public class StoreContext : DbContext
+	{
+		private readonly DbContextOptions<StoreContext> _options;
+
+		public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+		{
+			_options = options;
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			//modelBuilder.ApplyConfiguration( new ProductConfigurations() );
+
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+
+		public DbSet<Product> Products { get; set; }
+		public DbSet<ProductBrand> ProductBrands { get; set; }
+		public DbSet<ProductCategory> ProductCategories { get; set; }
+	}
+}
