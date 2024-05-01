010ff12d214087db19a291ff70805c2f98a0f921
youssefessam117 <johntiastoo@gmail.com>
2024-05-01 13:35:49 +0300

    Auth Service - JWT Token Creation

--- Route.Talabat.APIs/Dtos/RegisterDto.cs
+++ Route.Talabat.APIs/Dtos/RegisterDto.cs
@@ -15,8 +15,17 @@ namespace Route.Talabat.APIs.Dtos
 		public string Phone {  get; set; }
 
 		[Required]
-		[RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&lt;,])(?!.*\\s)*$",
+		[RegularExpression("(?=^.{6,10}$)((?=.*\\d))(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;,])(?!.*\\s).*$",
 			ErrorMessage = "Password must have 1 Uppercase 1 lowercase 1 number, 1 non alphanumeric and at least 6 characters")]
 		public string Password { get; set; }
 	}
 }
+
+
+
+
+
+
+
+
+
