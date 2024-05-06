9de4e13df278ef931f5b1a1f7544bc954a90b02d
youssefessam117 <johntiastoo@gmail.com>
2024-05-06 12:55:39 +0300

    order Module - Configuration Classes

---
+++ Talabat.Infrastructure/Data/Config/Order Config/OrderConfigurations.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entites.Order_Aggregate;
+
+namespace Talabat.Infrastructure.Data.Config.Order_Config
+{
+	internal class OrderConfigurations : IEntityTypeConfiguration<Order>
+	{
+		public void Configure(EntityTypeBuilder<Order> builder)
+		{
+			builder.OwnsOne(order => order.ShippingAddress, shippingAddress => shippingAddress.WithOwner());
+
+			builder.Property(order => order.Status)
+				.HasConversion(
+				(Ostatus) => Ostatus.ToString(),
+				(Ostatus) => (OrderStatus)Enum.Parse(typeof(OrderStatus), Ostatus)
+				);
+
+			builder.Property(order => order.Subtotal)
+				.HasColumnType("decimal(12,2)");
+		}
+	}
+}
