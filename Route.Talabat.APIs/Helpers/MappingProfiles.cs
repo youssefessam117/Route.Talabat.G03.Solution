47f6f168f688266fb09d7ce4f8639e74f7825934
youssefessam117 <johntiastoo@gmail.com>
2024-05-08 02:23:24 +0300

    OrderItemPictureUrlResolver

--- Route.Talabat.APIs/Helpers/MappingProfiles.cs
+++ Route.Talabat.APIs/Helpers/MappingProfiles.cs
@@ -29,7 +29,8 @@ namespace Route.Talabat.APIs.Helpers
 			CreateMap<OrderModule.OrderItem, OrderItemDto>()
 				.ForMember(d => d.ProductId, o => o.MapFrom(s => s.Product.ProductId))
 				.ForMember(d => d.ProductName, o => o.MapFrom(s => s.Product.ProductName))
-				.ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl));
+				.ForMember(d => d.PictureUrl, o => o.MapFrom(s => s.Product.PictureUrl))
+				.ForMember(d => d.PictureUrl, o => o.MapFrom<OrderItemPictureUrlResolver>());
 
 		}
 	}
